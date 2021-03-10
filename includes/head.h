d9c1dd50f29cff60885fefa4477dfc5c1cd1dcd8
ezalos <ezalos@gmail.com>
2021-03-10 12:15:46 +0100

    Parse print w/ colors

--- includes/head.h
+++ includes/head.h
@@ -6,7 +6,7 @@
 /*   By: ezalos <ezalos@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/11/12 11:15:02 by ldevelle          #+#    #+#             */
-/*   Updated: 2021/03/07 17:44:02 by ezalos           ###   ########.fr       */
+/*   Updated: 2021/03/10 12:09:23 by ezalos           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -96,6 +96,23 @@ typedef struct	s_packer
 
 	uint64_t		size;
 }					t_packer;
+
+#define _RED "\x1b[31m"
+#define _GREEN "\x1b[32m"
+#define _BLUE "\x1b[34m"
+#define _YELLOW "\x1b[33m"
+#define _MAGENTA "\x1b[35m"
+#define _CYAN "\x1b[36m"
+#define _RESET "\x1b[0m"
+#define _BROWN "\x1b[38;2;238;205;163m"
+#define _PINK "\x1b[38;2;239;100;100m"
+#define _PURPLE "\x1b[38;2;101;78;163m"
+#define _ORANGE "\x1b[38;2;155;75;43m"
+#define _TURQUOISE "\x1b[38;2;68;140;121m"
+#define CU_RESET "\033[0;0H"
+#define CU_LOAD "\033[u"
+#define CU_SAVE "\033[s"
+#define CL_SCREEN "\033[2J"
 
 # include "prototypes_woody_woodpacker.h"
 
--- srcs/parse_elf.c
+++ srcs/parse_elf.c
@@ -6,7 +6,7 @@
 /*   By: ezalos <ezalos@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/02/19 16:13:04 by ezalos            #+#    #+#             */
-/*   Updated: 2021/03/10 00:55:57 by ezalos           ###   ########.fr       */
+/*   Updated: 2021/03/10 12:15:24 by ezalos           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -95,19 +95,19 @@ void 	parse_print(t_packer *packer)
 	while (packer->phdr_array[++i_p])
 	{
 		phdr = packer->phdr_array[i_p]->phdr;
-		printf("Phdr %2d\t\tLoad: %s\n", i_p, phdr->p_type & PT_LOAD ? "True" : "False");
+		printf("%sPhdr %2d [%2d]%s\t%sLoad: %s%s\n", _GREEN, i_p, get_program_header_index(packer, packer->phdr_array[i_p]->phdr), _RESET, _YELLOW, phdr->p_type & PT_LOAD ? "True" : "False", _RESET);
 		printf("Start 0x%lx\tSegment size   %ld\n", phdr->p_vaddr, phdr->p_memsz);
-		printf("End   0x%lx\tAvailable size %d\n\n", phdr->p_memsz + phdr->p_vaddr, packer->phdr_array[i_p]->available_size);
+		printf("End   0x%lx\t%sAvailable size %d%s\n\n", phdr->p_memsz + phdr->p_vaddr, _CYAN, packer->phdr_array[i_p]->available_size, _RESET);
 		if (!packer->phdr_array[i_p]->shdr_array)
 			continue;
 		i_s = -1;
 		while (packer->phdr_array[i_p]->shdr_array[++i_s])
 		{
 			shdr = packer->phdr_array[i_p]->shdr_array[i_s]->shdr;
-			printf("\tShdr %d [%d]:\t%s\n", i_s, get_section_header_index(packer, shdr), get_sec_name(packer, shdr));
-			printf("\tAlloc: %s\tExec: %s\n", shdr->sh_flags & SHF_ALLOC ? "True" : "False", shdr->sh_flags & SHF_EXECINSTR ? "True" : "False");
+			printf("\t%sShdr %2d [%2d]:\t%s%s\n", _GREEN, i_s, get_section_header_index(packer, shdr), get_sec_name(packer, shdr), _RESET);
+			printf("\t%sAlloc: %s\tExec: %s%s\n", _YELLOW,shdr->sh_flags & SHF_ALLOC ? "True" : "False", shdr->sh_flags & SHF_EXECINSTR ? "True" : "False", _RESET);
 			printf("\tStart 0x%lx\tSection size   %ld\n", shdr->sh_offset, shdr->sh_size);
-			printf("\tEnd   0x%lx\tAvailable size %d\n\n", shdr->sh_size + shdr->sh_offset, packer->phdr_array[i_p]->shdr_array[i_s]->available_size);
+			printf("\tEnd   0x%lx\t%sAvailable size %d%s\n\n", shdr->sh_size + shdr->sh_offset, _CYAN, packer->phdr_array[i_p]->shdr_array[i_s]->available_size, _RESET);
 		}
 	}
 }
