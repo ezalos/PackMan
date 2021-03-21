7d751fab9ee11095e9ce335792710798a1a89cad
root <rkirszba@student.42.fr>
2021-03-21 12:47:38 +0000

    fix: add a new line at the end of get_zones

---
+++ btc_asm/end_64.asm
@@ -0,0 +1,21 @@
+section .text
+
+btc_end:
+	add rsp, 0x118
+	popf
+	pop r15
+	pop r14
+	pop r13
+	pop r12
+	pop r11
+	pop r10
+	pop r9
+	pop r8
+	pop rdi
+	pop rsi
+	pop rdx
+	pop rcx
+	pop rbx
+	pop rax
+	pop rsp
+	pop rbp
