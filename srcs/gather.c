06f704f8984db66ce5537918fe3b3005f0da0212
ezalos <ezalos@gmail.com>
2021-03-25 23:23:22 +0100

    SPRING CLEANING

--- srcs/gather.c
+++ srcs/gather.c
@@ -1,35 +1,31 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   main.c                                             :+:      :+:    :+:   */
+/*   gather.c                                           :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: ezalos <ezalos@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2020/11/29 11:21:04 by ezalos            #+#    #+#             */
-/*   Updated: 2021/02/19 11:1:0128 by ezalos           ###   ########.fr       */
+/*   Created: 2021/03/25 23:23:07 by ezalos            #+#    #+#             */
+/*   Updated: 2021/03/25 23:23:14 by ezalos           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "head.h"
 
-int			main(int ac, char **av)
+int8_t		gather_all_infos(t_packer *packer)
 {
-	t_packer	packer;
+	cave_gathering(packer);
 
-	if (ac < 2)
-		return (print_usage(av[0]));
+	packer->to_crypt = get_zones(packer, PT_LOAD, PF_R, &data_filler_zone_to_crypt);
+	packer->caves = get_zones(packer, PT_LOAD, PF_X | PF_R, &data_filler_cave);
 
-	if (FAILURE == init(&packer, av))
-		return (EXIT_FAILURE);
+	if (DEBUG == 2)
+	{
+		print_zones(packer->to_crypt);
+		print_zones(packer->caves);
+	}
 
-	if (FAILURE == gather_all_infos(&packer))
-		return (EXIT_FAILURE);
-
-	if (FAILURE == chirurgy(&packer))
-		return (EXIT_FAILURE);
-
-	if (FAILURE == save_woody(&packer))
-		return (EXIT_FAILURE);
-
-	return (EXIT_SUCCESS);
+	if (FALSE == (packer->to_crypt && packer->caves))
+		return (FAILURE);
+	return (SUCCESS);
 }
