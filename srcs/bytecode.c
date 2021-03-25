ccb606769f9f111afd73e87af3bd8a2f3fc51602
ezalos <ezalos@gmail.com>
2021-03-26 00:35:17 +0100

    Cleaner bytecode + bytecode_blueprint created

--- srcs/bytecode.c
+++ srcs/bytecode.c
@@ -12,24 +12,36 @@
 
 #include "head.h"
 
+#define		MINIMAL_WOODY	FALSE
+
 ssize_t		chirurgy(t_packer *packer)
 {
 	t_dlist	*blueprint;
 	ssize_t ret;
 
 	logging("\n*** %s: Creating blueprint\n", __func__);
-
-	//Segfault in woody
-	blueprint = blueprint_creation(packer);
-	//NO Segfault in woody
-	// blueprint = blueprint_minimal();
-
-	logging("\n*** %s: Solving injection\n", __func__);
-	ret = solve_bytecodes(packer, packer->caves, blueprint, TRUE);
-	if (ret != FAILURE)
+	if (MINIMAL_WOODY)
 	{
-		((Elf64_Ehdr *)packer->content)->e_entry = ret;
+		//NO Segfault in woody
+		blueprint = blueprint_minimal();
 	}
-	logging("\n*** %s: ret= %ld\n", __func__, ret);
-	return (ret);
+	else
+	{
+		//Segfault in woody
+		blueprint = blueprint_creation(packer);
+	}
+	logging("\n*** %s: Solving injection\n", __func__);
+	if (FAILURE == (ret = solve_bytecodes(packer, packer->caves, blueprint, TRUE)))
+		return (FAILURE);
+
+	logging("\n*** %s: Entry Point: %ld [%lx]\n", __func__, ret, ret);
+	((Elf64_Ehdr *)packer->content)->e_entry = ret;
+
+	if (!MINIMAL_WOODY)
+	{
+		logging("\n*** %s: Cypher crypt zones", __func__);
+		crypt_zones(packer);
+	}
+
+	return (SUCCESS);
 }
