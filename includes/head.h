5851b2832240e500ae14d67d583eae5de5116f86
ezalos <ezalos@gmail.com>
2021-03-26 10:41:14 +0100

    DEBUG == 0

--- includes/head.h
+++ includes/head.h
@@ -6,7 +6,7 @@
 /*   By: ezalos <ezalos@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/11/12 11:15:02 by ldevelle          #+#    #+#             */
-/*   Updated: 2021/03/26 10:37:49 by ezalos           ###   ########.fr       */
+/*   Updated: 2021/03/26 10:40:45 by ezalos           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -27,7 +27,7 @@
 # include <unistd.h>
 # include <time.h>
 
-# define DEBUG		2
+# define DEBUG		0
 extern int debug_level;
 
 # define SUCCESS	0
