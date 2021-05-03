8335a886c5a3deffe9b18f38c5c6fcc0be1e6738
root <rkirszba@student.42.fr>
2021-05-03 19:03:47 +0200

    Checking code -> get_zones() + respect_sacred_memory_size()

--- srcs/gather.c
+++ srcs/gather.c
@@ -6,7 +6,7 @@
 /*   By: ezalos <ezalos@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/03/25 23:23:07 by ezalos            #+#    #+#             */
-/*   Updated: 2021/05/02 11:28:19 by ezalos           ###   ########.fr       */
+/*   Updated: 2021/05/03 18:57:24 by ezalos           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -32,13 +32,11 @@ void		zones_add_rights_to_used_caves(t_list *zone, Elf64_Word rights)
 	}
 }
 
-void		check_zones_to_crypt(t_packer *packer)
+void		respect_sacred_memory_size(t_packer *packer, t_list **to_crypt)
 {
-	t_list	**to_crypt;
 	t_list	*tmp;
 	t_zone	*zone;
 
-	to_crypt = &packer->to_crypt;
 	while (*to_crypt)
 	{
 		zone = (*to_crypt)->data;
@@ -64,10 +62,18 @@ int8_t		gather_all_infos(t_packer *packer)
 	if (TRUE == cave_gathering(packer))
 		return (FAILURE);
 
+	// TODO: in both case check if it has returned null
+	// error1: no executable segments
+	// error2: no loadable segments
 	packer->to_crypt = get_zones(packer, PT_LOAD, PF_X | PF_R, &data_filler_zone_to_crypt);
 	// packer->to_crypt = get_zones(packer, PT_LOAD, 0, &data_filler_zone_to_crypt);
 	packer->caves = get_zones(packer, PT_LOAD, 0, &data_filler_cave);
 
+	if (FALSE == (packer->to_crypt && packer->caves))
+		return (FAILURE); // a revoir
+	// TODO: undefine behavior ou on laisse tel quel
+	// TODO: louis check system ABI V if 1st load + exec is necessarly .text
+	// TODO: sinon free a partir de packer->to_crypt->next + next = NULL
 	packer->to_crypt->next = NULL;
 
 	if (debug_level == 2)
@@ -77,10 +83,8 @@ int8_t		gather_all_infos(t_packer *packer)
 	}
 
 
-	if (FALSE == (packer->to_crypt && packer->caves))
-		return (FAILURE);
 	// packer->to_crypt->next = NULL;
-	check_zones_to_crypt(packer);
+	respect_sacred_memory_size(packer, &(packer->to_crypt));
 	zones_add_rights_to_crypt(packer->to_crypt, PF_W);
 	//just for test purpose:
 	// t_list *curs;
